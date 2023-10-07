844f59e5f337f8d6455a61dc6f706ceb8dd97451
Nagib <131501717+NicolasNagib653@users.noreply.github.com>
2023-10-07 19:56:37 -0300

    Initial commit

---
+++ scripts/desafio.py
@@ -0,0 +1,4 @@
+dia = input ('Dia')
+mes = input ('Mês')
+ano  = input('Ano')
+print('Fico muito feliz em saber que você nasceu no dia', dia ,'/', mes,'/',ano)
---
+++ scripts/desafio02.py
@@ -0,0 +1,4 @@
+num1 = input('Primeiro número')
+num2 = input('Segundo número')
+num3 = str(num1 + num2)
+print(num3)
